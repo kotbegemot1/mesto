542f76f23fe9af3fa18c4b261d94a9212bb64d51
kotbegemot1 <kk1slorod@gmail.com>
2023-01-31 18:59:27 +0300

    fix: fix bugs

---
+++ scripts/index.js
@@ -0,0 +1,37 @@
+// переменные профайла
+let nameProfile = document.querySelector('.profile__name');
+let descriptionProfile = document.querySelector('.profile__description');
+let editProfile = document.querySelector('.profile__edit');
+// переменные попап
+let popupTarget = document.querySelector('.popup');
+let popupFormTarget = popupTarget.querySelector('.popup__form');
+let closePopupBtn = popupFormTarget.querySelector('.popup__abort-button');
+let saveInfoBtn = popupFormTarget.querySelector('.popup__save-button');
+// инпуты попап
+let inputName = popupFormTarget.querySelector('.popup__edit-info_field_name')
+let inputDescription = popupFormTarget.querySelector('.popup__edit-info_field_description')
+
+// функция открытия попапа
+let handleEditButtonClick = function () {
+  popupTarget.classList.add('popup_open');
+  inputName.value = nameProfile.textContent;
+  inputDescription.value = descriptionProfile.textContent;
+}
+
+// функция закрытия попапа
+let handleCloseButtonClick = function () {
+  popupTarget.classList.remove('popup_open');
+}
+
+// функиция сохранения информации
+let saveInfoButtonClick = function (evt) {
+  evt.preventDefault();
+  nameProfile.textContent = inputName.value;
+  descriptionProfile.textContent = inputDescription.value;
+  popupTarget.classList.remove('popup_open');
+}
+
+// слушатели
+editProfile.addEventListener('click', handleEditButtonClick);
+closePopupBtn.addEventListener('click', handleCloseButtonClick);
+popupFormTarget.addEventListener('submit', saveInfoButtonClick);
